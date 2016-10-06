82ad721959437e9fcc3934881c15655853853a22
LOUGE\nick <nick@canadix.ch>
2016-10-07 00:40:54 +0200

    hash table

---
+++ src/Hash-table.ps1
@@ -0,0 +1,16 @@
+<#
+	Hash Table
+#>
+
+$array_name = @{}
+
+array_name = @{key1 = item1; key2 = item2;...}
+
+$usa_states=@{ CA="California";
+  "NY" = "New York";
+  "IL" = "Illinois";
+  "NH" = "New Hampshire"}
+  
+$usa_states.add("GA","Goregia")
+
+$usa_states.Set_Item("GA", "Georgia")
