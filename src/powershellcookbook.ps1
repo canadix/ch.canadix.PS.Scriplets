b4a79eabd235d0057d0b1216a6cfe48a4c85cb46
LOUGE\nick <nick@canadix.ch>
2016-11-16 01:30:41 +0100

    o'reiles

---
+++ src/powershellcookbook.ps1
@@ -0,0 +1,19 @@
+﻿get-process | where-object {$_.handles -ge 500 } | Sort-Object handles | Format-Table Handles,name,description -auto
+
+
+# Ubiquitous Scripting
+$HandleCount = 0
+foreach  ($process in Get-Process) { $HandleCount += $process.handles }
+$HandleCount
+
+#firewall
+$firewall = New-Object -com HNetCfg.FwMgr
+$firewall.LocalPolicy.CurrentProfile
+
+Set-Location HKCU:\Software\Microsoft\Windows\
+
+
+Get-ItemProperty .
+
+Set-Location cert:\CurrentUser\Root
+Get-ChildItem
