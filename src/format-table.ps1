7d24b1b745fb87c6b6fca7c35343b297979b3560
LOUGE\nick <nick@canadix.ch>
2017-01-17 07:49:30 +0100

    format-table

---
+++ src/format-table.ps1
@@ -0,0 +1,9 @@
+﻿Get-Process | Format-Table -Property Name,id,vm,@{n='pm(mb)';e={$_.pm / 1mb -as [int]}} -AutoSize
+
+
+Get-Service | Sort status -desc | Format-Table -GroupBy Status
+
+
+Format-List 
+
+Get-WmiObject -Class win32_logicaldisk | Format-List -Property *
